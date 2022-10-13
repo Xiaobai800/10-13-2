7c720fecff6c32fca95476fced86833ab6210620
Xiaobai800 <2533760032@qq.com>
2022-10-13 22:15:29 +0800

    添加项目文件。

---
+++ 10 13  2/10 13 2.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdlib.h>
+//int main()  
+//{
+//	int i = 0;
+//	for (i = 0; i <= 100; i++)
+//	{
+//		printf("%d ", i);
+//	}for (i = 0; i <= 100; i++)
+//	{
+//		printf("%d ", 10-i);
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int main()
+//{
+//	printf("hello world!");
+//	int x = 10;
+//	int y = 20;
+//	int c = Add(x, y);
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		int ret = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+int main()
+{
+	int i = 0;
+	int arr[10] = { 0 };
+	for (i = 0; i <= 11; i++)
+	{
+		arr[i] = i;
+		printf("hehe\n");
+	}
+	return 0;
+}
